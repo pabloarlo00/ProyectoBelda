8bb57106e78b1b19273dda2677f13b50b31636ff
Pablo <pabloarlo00@gmail.com>
2026-02-20 14:16:46 +0100

    creada interface en el backend

---
+++ backend/backend/src/noticias/interfaces/noticias.interface.ts
@@ -0,0 +1,24 @@
+export interface SeccionInterface {
+  nombre: string;
+  iconoWeb: string;
+  iconoApp: string;
+}
+
+export interface ComentarioInterface {
+  nombre: string;
+  email: string;
+  comentario: string;
+  fecha?: Date;
+}
+
+export interface NoticiaInterface {
+  _id?: string;
+  imagenes: string[];
+  titulo: string;
+  subtitulo?: string;
+  seccion: SeccionInterface;
+  autor: string;
+  fecha?: Date;
+  contenido: string;
+  comentarios?: ComentarioInterface[];
+}
